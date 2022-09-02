2ee7003582e959754a2c4ed2360f43a72a7f2896
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2022-09-02 11:11:26 +0200

    minor update

--- createScript.sh
+++ createScript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /chemotion/app && echo "User.create(email:'$email', password:'${2:-chemotion}', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5').save" | rails c && cd 
+cd /chemotion/app && echo "User.create(email:'$1', password:'${2:-chemotion}', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5').save" | rails c && cd 
