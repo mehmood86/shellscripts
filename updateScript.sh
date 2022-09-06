a7ff318dd82bba5737d744efe653198310adeea4
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2022-09-06 17:37:10 +0200

    Update updateScript.sh

--- updateScript.sh
+++ updateScript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /chemotion/app && echo "User.find_by_email('$1').update((password:'${2:-chemotion}', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5')" | rails c && cd ..
+cd /chemotion/app && echo "User.find_by_email('$1').update(password:'${2:-chemotion}', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5')" | rails c && cd ..
