b6bd7edd776b2a530909f170002fc09b113b616c
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2022-09-12 12:47:44 +0200

    Update updateScript.sh

--- updateScript.sh
+++ updateScript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /chemotion/app && echo "User.find_by_email('$1').update(password:'${2:-chemotion}', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5')" | rails c | tail -2 | head -1 && cd ..
+if [[ $(cd /chemotion/app && echo "User.find_by(email:'$1')" | rails c | tail -2 | head -1) == 'nil' ]] ; then echo "No record found"; else $(cd /chemotion/app && echo "User.find_by(email:'test1@email.com').update(password:'$2', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5')" | rails c ); fi
