ecfa24addc8652748c4784c4ba6aab6391ccbd6c
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2022-09-12 13:41:53 +0200

    Update updateScript.sh

--- updateScript.sh
+++ updateScript.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-if [[ $(cd /chemotion/app && echo "User.find_by(email:'$1')" | rails c | tail -2 | head -1) == 'nil' ]] ; then echo "No record found"; else $(echo "User.find_by(email:'test1@email.com').update(password:'$2', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5')" | rails c ); fi
+CMDStatement1=`cd /chemotion/app && echo "User.find_by(email:'$1')" | rails c | tail -2 | head -1 && cd ../..`
+CMDStatement2=`cd /chemotion/app && echo "User.find_by(email:'$1').update(password:'$2', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5')" | rails c ) && cd ../..`
+
+if [ $(CMDStatement1) == 'nil' ] ; then echo "No record found"; elif [ $( CMDStatement2) | head -1 == 'true' ]; echo "Record has been successfuly updated" ; else echo "Something went wrong" ; fi
