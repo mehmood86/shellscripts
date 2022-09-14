30045ecc70bceace96596e4d2aebbb0b56d8a1c3
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2022-09-14 16:08:05 +0200

    Update updateScript.sh

--- updateScript.sh
+++ updateScript.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 cd /chemotion/app
-ifExists=`echo "User.find_by(email:'$1')" | rails c | tail -2 | head -1`
-if [ $ifExists=='nil' ]; then echo "No record found"; else \
-echo "User.find_by(email:'$1').update(password:'$2', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5')" | rails c | tail -1;
-echo "User updated"; fi
+
+ifEmailExists=$(echo "User.find_by(email:'$1')" | rails c | tail -2 | head -1)
+
+if [ $ifEmailExists=="nil" ]; then
+    echo "No record found"
+else
+    userUpdated=$(echo "User.find_by(email:'$1').update(password:'$2', first_name:'$3', last_name:'$4', type:'Person', name_abbreviation:'$5')" | rails c | tail -2 | head -1)
+    if [[ "$userUpdated" == 'true' ]]; then
+        echo "User updated successfuly"
+    else
+        echo "Please use unique abbreviation"
+    fi
+fi
