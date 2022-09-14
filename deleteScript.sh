f7fa14efbd558b905bb1171b28da9cabe6abe91c
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2022-09-14 16:45:13 +0200

    Update deleteScript.sh

--- deleteScript.sh
+++ deleteScript.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 cd /chemotion/app
-ifExists=`echo "User.find_by(email:'$1')" | rails c | tail -2 | head -1`
-if [ $ifExists=='nil' ]; then echo "No record found"; else \
-echo "User.find_by(email:'$1').destroy" | rails c && echo "User deleted"; fi
+
+ifEmailExists=$(echo "User.find_by(email:'$1')" | rails c | tail -2 | head -1)
+
+if [ $ifEmailExists=="nil" ]; then
+    echo "No record found"; 
+else
+    echo "User.find_by(email:'$1').destroy" | rails c | tail -1 && echo "User deleted"; fi
