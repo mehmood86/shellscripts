f68cfc4ba4cdbdac2ac5f40fb8b841af9b190f91
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2022-09-14 16:48:11 +0200

    Update deleteScript.sh

--- deleteScript.sh
+++ deleteScript.sh
@@ -7,4 +7,4 @@ ifEmailExists=$(echo "User.find_by(email:'$1')" | rails c | tail -2 | head -1)
 if [ "$ifEmailExists" == "nil" ]; then
     echo "No record found"; 
 else
-    echo "User.find_by(email:'$1').destroy" | rails c | tail -1 && echo "User deleted"; fi
+    echo "User.find_by(email:'$1').destroy" | rails c | tail -1 && echo "User deleted succesfuly."; fi
